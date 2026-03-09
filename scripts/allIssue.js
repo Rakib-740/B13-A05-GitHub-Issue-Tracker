8a5dbf620ccab4c5b21a68bb46354dbcffc7d151
Md Rakibuzzaman <rakibuzzaman740@gmail.com>
2026-03-09 11:45:23 +0600

    modal added

--- scripts/allIssue.js
+++ scripts/allIssue.js
@@ -6,14 +6,13 @@ async function loadIssue() {
   const res = await fetch(
     "https://phi-lab-server.vercel.app/api/v1/lab/issues",
   );
-  
+
   const json = await res.json();
   stopLoading();
   //   console.log(json.data);
   issueData = json.data;
   document.getElementById("all-btn").click();
-  displayIssues(issueData);
-  
+  // displayIssues(issueData);
 }
 
 const displayIssues = (issues) => {
@@ -24,25 +23,37 @@ const displayIssues = (issues) => {
   issues.forEach((issue) => {
     const issueCard = document.createElement("div");
 
-    issueCard.className = `flex flex-col gap-3 shadow-sm rounded-lg border-t-4 ${issue.status === 'open' 
-      ? "border-t-[rgb(99,230,190)]" 
-      : "border-t-[rgb(177,151,252)]"}`;
+    issueCard.className = `flex flex-col gap-3 shadow-sm rounded-lg border-t-4 ${
+      issue.status === "open"
+        ? "border-t-[rgb(99,230,190)]"
+        : "border-t-[rgb(177,151,252)]"
+    }`;
+    issueCard.onclick = () => issueModal(issue.id);
 
     issueCard.innerHTML = `
         <div>
             <div class="flex flex-col p-4 gap-3 shadow-sm">
               <div class="flex justify-between">
-                ${issue.status === "open"
-                    ?`<img src="./assets/Open-Status.png" alt=""/>`
-                    :`<img src="./assets/Closed- Status .png" alt="">`
+                ${
+                  issue.status === "open"
+                    ? `<img src="./assets/Open-Status.png" alt=""/>`
+                    : `<img src="./assets/Closed- Status .png" alt="">`
                 }
-                <div class="badge badge-soft badge-warning ${issue.priority === "high" ? "bg-[#FEECEC] text-[#EF4444]" : issue.priority === "medium" ? "bg-[#FFF6D1] text-[#F59E0B]" : "bg-[#EEEFF2] text-[#9CA3AF]"}">${
+                <div class="badge badge-soft badge-warning 
+                ${
                   issue.priority === "high"
-                  ? "HIGH"
-                  : issue.priority === "medium"
-                  ? "MEDIUM"
-                  : "LOW"
-                }</div>
+                    ? "bg-[#FEECEC] text-[#EF4444]"
+                    : issue.priority === "medium"
+                      ? "bg-[#FFF6D1] text-[#F59E0B]"
+                      : "bg-[#EEEFF2] text-[#9CA3AF]"
+                }">
+                  ${
+                    issue.priority === "high"
+                      ? "HIGH"
+                      : issue.priority === "medium"
+                        ? "MEDIUM"
+                        : "LOW"
+                  }</div>
               </div>
               <div class="">
                 <h2 class="text-[14px] text-[#1F2937] font-semibold mb-2">
@@ -70,13 +81,12 @@ const displayIssues = (issues) => {
 // label badge create function
 
 const labels = (items) => {
-  const label = items.map(item =>
-    `<div class="badge badge-soft badge-warning">${item.toUpperCase()}</div>`
+  const label = items.map(
+    (item) =>
+      `<div class="badge badge-soft badge-warning">${item.toUpperCase()}</div>`,
   );
 
   return label.join(" ");
-}
-
-
+};
 
 loadIssue();
---
+++ scripts/modal.js
@@ -0,0 +1,55 @@
+// function to show modal
+
+async function issueModal(id) {
+  const res = await fetch(
+    `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`,
+  );
+  const json = await res.json();
+  const issue = json.data;
+
+  const modalContainer = document.getElementById("modal-container");
+  const modal = document.getElementById("modal-issue");
+
+  modalContainer.innerHTML = `
+        <div class="space-y-3">
+                <h3 class="font-bold text-2xl text-[#1F2937]">${issue.title}</h3>
+                <div class="flex items-center gap-2">
+                    <button class="px-4 py-2 text-white rounded-full ${issue.status === "open" ? "bg-[#00A96E]" : "bg-[#A855F7]"}">${issue.status === "open" ? "Opened" : "Closed"}</button>
+                    <span class="text-[#64748B]"> • </span>
+                    <p class="text-xs text-[#64748B]">Opened by ${issue.author}</p>
+                    <span class="text-[#64748B]"> • </span>
+                    <p class="text-xs text-[#64748B]">${new Date(issue.createdAt).toLocaleDateString()}</p>
+                </div>
+                <div class="flex gap-1 items-center whitespace-nowrap">
+                    ${labels(issue.labels)}
+                </div>
+                <p class="text-[#64748B]">${issue.description}</p>
+                <div class="bg-[#F8FAFC] rounded-lg p-4 flex justify-between shadow-sm">
+                    <div>
+                        <p class="text-[#64748B]">Assignee:</p>
+                        <h3 class="text-[#1F2937] font-semibold">${issue.assignee === "" ? "Unknown" : issue.assignee}</h3>
+                    </div>
+                    <div class="flex flex-col justify-center items-center">
+                        <p class="text-[#64748B]">Priority:</p>
+                        <h3 class="rounded-full w-fit px-4 py-2 text-white 
+                        ${
+                          issue.priority === "high"
+                            ? "bg-[#EF4444]"
+                            : issue.priority === "medium"
+                              ? "bg-[#F59E0B]"
+                              : "bg-[#9CA3AF]"
+                        }">
+                            ${
+                              issue.priority === "high"
+                                ? "HIGH"
+                                : issue.priority === "medium"
+                                  ? "MEDIUM"
+                                  : "LOW"
+                            }</h3>
+                    </div>
+                </div>
+            </div>
+    `;
+
+  modal.showModal();
+}
